66f8cd6e063ad3ec3bebb2098dfc86240ec5fc7e
Ben <642499128@qq.com>
2018-11-24 19:43:23 +0800

    新增属性

--- Hello.java
+++ Hello.java
@@ -1,10 +1,15 @@
 public class Hello{
 	
+	private String aaa;
+	
+	private String bbb;
+	
 	public static void main(String[] args){
 		System.out.println('Hello Git World');
 		System.out.println('Haha');
 		System.out.println('bbb');
 		System.out.println('aaaa');
+		
 	}
 	
 }
